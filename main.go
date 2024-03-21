9ec3a4221c9128fb27c4038b8ca319863af99a5b
jozef <jozef.reisinger@gmail.com>
2024-03-21 09:46:41 +0100

    use shorter name

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
-// Bytecounter implements a (concrete) type whose method counts bytes before
+// Bcounter implements a (concrete) type whose method counts bytes before
 // discarding them. Since it satisfies the io.Writer interfaces (an abstract type),
 // we can pass it to fmt.Fprint.
 package main
 
 import "fmt"
 
-type ByteCounter int
+type bCounter int
 
-func (c *ByteCounter) Write(p []byte) (int, error) {
-	*c := ByteCounter(len(p)) // must explicitly convert int to ByteCounter
+func (b *bCounter) Write(p []byte) (int, error) {
+	*b += bCounter(len(p)) // must explicitly convert int to bCounter
 	return len(p), nil
 }
 
 func main() {
-	var c ByteCounter
-	c.Write([]byte("hello")) // c == 5
-	c = 0                    // reset the counter
-	fmt.Fprint(&c, "world")  // c == 5
+	var b bCounter
+	b.Write([]byte("hello")) // b == 5
+	b = 0                    // reset the counter
+	fmt.Fprint(&b, "world")  // b == 5
 }
