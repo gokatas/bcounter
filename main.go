2fcd5f165acf6c676f136c294b9ae2aba45298fc
jozef <jozef.reisinger@gmail.com>
2024-03-21 14:39:48 +0100

    make it shorter :-)

--- main.go
+++ main.go
@@ -15,12 +15,9 @@ func (b *bcounter) Write(p []byte) (int, error) {
 
 func main() {
 	var b bcounter // declare variable of type bcounter
-
 	b.Write([]byte("hello"))
 	fmt.Printf("%v is %v bytes: %v\n", "world", b, []byte("world"))
-
 	b = 0 // reset the byte counter
-
 	katas := "形"
 	fmt.Fprint(&b, katas)
 	fmt.Printf("%v is %v bytes: %v\n", katas, b, []byte(katas))
